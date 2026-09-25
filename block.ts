b2eb77460ba68fe7eb6b637f7237a27268d99044
simoneguptaa <simone.gupta03@gmail.com>
2026-09-25 10:47:15 +1000

    add function to calculate hash, install node @types/node and crypto-js packages, add gitignore @types/node - installed as a dev dependency. it's only needed by the ts compiler. ts gets compiled to js, the js runs in the node.js environment, which has the declarations and executable code built-in -> it's wasteful to ship it as a dependency for the node.js environment.

--- block.ts
+++ block.ts
@@ -13,3 +13,12 @@ class Block{
         this.hash = hash;
     }
 }
+
+var CryptoJS = require("crypto-js");
+
+// the block hash has nothing to do with mining yet, as there is no proof of work problem to solve
+const calculateHash = (index: number, data: string, timestamp: number, previousHash: string, hash: string) => {
+    CryptoJS.SHA256(index + data + timestamp + previousHash).toString();
+}
+
+// the deeper/ earlier the block is in a chain, the harder it is to modify it - hashes of all consecutive blocks must be changed
